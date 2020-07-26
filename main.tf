d5bac7f4962c27c8e5a569a2c2f7622fd6097918
Yuki Hattori <yuki.hattori@outlook.com>
2020-07-27 08:58:54 +0900

    comment out azure network watcher

--- main.tf
+++ main.tf
@@ -280,11 +280,12 @@ resource "azurerm_log_analytics_workspace" "loganalytics" {
   retention_in_days   = 30
 }
 #create network watcher
-resource "azurerm_network_watcher" "watcher" {
-  name                = "demo-nwwatcher"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
+# resource "azurerm_network_watcher" "watcher" {
+#   name                = "demo-nwwatcher"
+#   location            = var.location
+#   resource_group_name = azurerm_resource_group.rg.name
+# }
+
 /* #create network security groups Bastion Subnet
 resource "azurerm_network_security_group" "nsgbastion" {
   name                = "nsg_azurebastion"
@@ -888,46 +889,47 @@ resource "azurerm_storage_account" "storageaccount" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
-resource "azurerm_network_watcher_flow_log" "watcher-flow-mgmt" {
-  network_watcher_name = azurerm_network_watcher.watcher.name
-  resource_group_name  = azurerm_resource_group.rg.name
+# resource "azurerm_network_watcher_flow_log" "watcher-flow-mgmt" {
+#   network_watcher_name = azurerm_network_watcher.watcher.name
+#   resource_group_name  = azurerm_resource_group.rg.name
+# 
+#   network_security_group_id = azurerm_network_security_group.nsgmgmt.id
+#   storage_account_id        = azurerm_storage_account.storageaccount.id
+#   enabled                   = true
+# 
+#   retention_policy {
+#     enabled = true
+#     days    = 7
+#   }
+# 
+#   traffic_analytics {
+#     enabled               = true
+#     workspace_id          = azurerm_log_analytics_workspace.loganalytics.workspace_id
+#     workspace_region      = azurerm_log_analytics_workspace.loganalytics.location
+#     workspace_resource_id = azurerm_log_analytics_workspace.loganalytics.id
+#   }
+# }
+# resource "azurerm_network_watcher_flow_log" "watcher-flow-web" {
+#   network_watcher_name = azurerm_network_watcher.watcher.name
+#   resource_group_name  = azurerm_resource_group.rg.name
+# 
+#   network_security_group_id = azurerm_network_security_group.nsgweb.id
+#   storage_account_id        = azurerm_storage_account.storageaccount.id
+#   enabled                   = true
+# 
+#   retention_policy {
+#     enabled = true
+#     days    = 7
+#   }
+# 
+#   traffic_analytics {
+#     enabled               = true
+#     workspace_id          = azurerm_log_analytics_workspace.loganalytics.workspace_id
+#     workspace_region      = azurerm_log_analytics_workspace.loganalytics.location
+#     workspace_resource_id = azurerm_log_analytics_workspace.loganalytics.id
+#   }
+# }
 
-  network_security_group_id = azurerm_network_security_group.nsgmgmt.id
-  storage_account_id        = azurerm_storage_account.storageaccount.id
-  enabled                   = true
-
-  retention_policy {
-    enabled = true
-    days    = 7
-  }
-
-  traffic_analytics {
-    enabled               = true
-    workspace_id          = azurerm_log_analytics_workspace.loganalytics.workspace_id
-    workspace_region      = azurerm_log_analytics_workspace.loganalytics.location
-    workspace_resource_id = azurerm_log_analytics_workspace.loganalytics.id
-  }
-}
-resource "azurerm_network_watcher_flow_log" "watcher-flow-web" {
-  network_watcher_name = azurerm_network_watcher.watcher.name
-  resource_group_name  = azurerm_resource_group.rg.name
-
-  network_security_group_id = azurerm_network_security_group.nsgweb.id
-  storage_account_id        = azurerm_storage_account.storageaccount.id
-  enabled                   = true
-
-  retention_policy {
-    enabled = true
-    days    = 7
-  }
-
-  traffic_analytics {
-    enabled               = true
-    workspace_id          = azurerm_log_analytics_workspace.loganalytics.workspace_id
-    workspace_region      = azurerm_log_analytics_workspace.loganalytics.location
-    workspace_resource_id = azurerm_log_analytics_workspace.loganalytics.id
-  }
-}
 #create azure front door 
 resource "azurerm_frontdoor" "azfrontdoor" {
   name                                         = "${var.frontdoorname}${var.env}"
